eabfefab41c26797df9ec879934a2e50cba5aff0
Ignacio Kairuz <ikairuz@fi.uba.ar>
2023-02-07 11:57:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
-def main():
-    st.title("Hello, Streamlit!")
-    st.write("This is a simple Streamlit app.")
-
-if __name__ == '__main__':
-    main()
-
 
 #st.title("Hello, Vercel!")
 
